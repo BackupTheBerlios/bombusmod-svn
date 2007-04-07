dab07f91c62c6feb849a6da5f7e72b41956f10a1
apatin <apatin@54b045c3-0521-0410-810d-baac06d7fe59>
2007-04-07 07:32:28 +0000

    new color scheme

--- full/src/ui/ColorScheme.java
+++ full/src/ui/ColorScheme.java
@@ -57,55 +57,41 @@ public class ColorScheme {
 	return instance;
     }
 
-    public static int BALLOON_INK          =0x000000;
-    public static int BALLOON_BGND         =0xffffe0;
-    
-    public static int LIST_BGND            =0xFFFFFF;
-    public static int LIST_BGND_EVEN       =0xffeeff;
-    public static int LIST_INK             =0x000000;
-    
-    public static int MSG_SUBJ             =0xa00000;
-    public static int MSG_HIGHLIGHT        =0x904090;
-    
-    public static int DISCO_CMD            =0x000080;
-    
-    public static int BAR_BGND             =0x0033ff;
-    public static int BAR_INK              =0x33ffff;
-    
-    public static int CONTACT_DEFAULT      =0x000000;
-    public static int CONTACT_CHAT         =0x39358b;
-    public static int CONTACT_AWAY         =0x008080;
-    public static int CONTACT_XA           =0x535353;
-    public static int CONTACT_DND          =0x800000;
-    
-    public static int GROUP_INK            =0x000080;
-    
-    public static int BLK_INK              =0xffffff;
-    public static int BLK_BGND             =0x000000;
+    public static int BALLOON_INK	=0x4866ad;
+    public static int BALLOON_BGND	=0xffffe0;
+    public static int LIST_BGND         =0xffffff;
+    public static int LIST_BGND_EVEN	=0xf8f0f0;
+    public static int LIST_INK          =0x000000;
+    public static int MSG_SUBJ          =0xa00000;
+    public static int MSG_HIGHLIGHT	=0x904090;
+    public static int DISCO_CMD         =0x000080;
+    public static int BAR_BGND          =0xa00000;
+    public static int BAR_INK           =0xffffff;
+    public static int CONTACT_DEFAULT	=0x000000;
+    public static int CONTACT_CHAT	=0x39358b;
+    public static int CONTACT_AWAY	=0x008080;
+    public static int CONTACT_XA	=0x535353;
+    public static int CONTACT_DND	=0x800000;
+    public static int GROUP_INK         =0x000080;
+    public static int BLK_INK           =0xffffff;
+    public static int BLK_BGND          =0x000000;
+    public static int MESSAGE_IN	=0x0000b0;
+    public static int MESSAGE_OUT	=0xb00000;
+    public static int MESSAGE_PRESENCE	=0x006000;
+    public static int MESSAGE_AUTH	=0x400040;
+    public static int MESSAGE_HISTORY	=0x535353;
+    public static int PGS_REMAINED	=0xffffff;
+    public static int PGS_COMPLETE	=0x0000ff;
+    public static int PGS_BORDER	=0x808080;
+    public static int PGS_BGND          =0x000000;
+    public static int HEAP_TOTAL	=0xffffff;
+    public static int HEAP_FREE         =0x00007f;
+    public static int CURSOR_BGND	=0xfae0e0;
+    public static int CURSOR_OUTLINE	=0xf5dbdb;
+    public static int SCROLL_BRD	=0x950d04;
+    public static int SCROLL_BAR	=0xbbbbbb;
+    public static int SCROLL_BGND	=0xffffff;
 
-    public static int MESSAGE_IN           =0x0000b0;
-    public static int MESSAGE_OUT          =0xb00000;
-    public static int MESSAGE_PRESENCE     =0x006000;
-    public static int MESSAGE_AUTH         =0x400040;
-    public static int MESSAGE_HISTORY      =0x535353;
-
-    public static int PGS_REMAINED         =0xffffff;
-    public static int PGS_COMPLETE         =0x0000ff;
-    public static int PGS_BORDER           =0x808080;
-    public static int PGS_BGND             =0x000000;
-    
-    public static int HEAP_TOTAL           =0xffffff;
-    public static int HEAP_FREE            =0x00007f;
-
-    public static int CURSOR_BGND          =0xC8D7E6;
-    public static int CURSOR_OUTLINE       =0x000066;
-
-    public static int SCROLL_BRD           =0x000000;
-    public static int SCROLL_BAR           =0xBBBBBB;
-    public static int SCROLL_BGND          =0xDDDDDD;
-    
-
-    
     protected void loadFromStorage(){
 	try {
 	    DataInputStream inputStream=NvStorage.ReadFileRecord("ColorDB", 0);
