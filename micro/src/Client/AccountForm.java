e95c4a6b7ed986920a4104040c4521c604d04998
apatin <apatin@54b045c3-0521-0410-810d-baac06d7fe59>
2007-08-30 18:40:29 +0000

    781

---
+++ micro/src/Client/AccountForm.java
@@ -0,0 +1,176 @@
+/*
+ * AccountForm.java
+ *
+ * Created on 20 Март 2005 г., 21:20
+ *
+ * Copyright (c) 2005-2006, Eugene Stahov (evgs), http://bombus.jrudevels.org
+ * All rights reserved.
+ */
+
+package Client;
+
+import javax.microedition.lcdui.Choice;
+import javax.microedition.lcdui.ChoiceGroup;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.ItemStateListener;
+import javax.microedition.lcdui.TextField;
+import locale.SR;
+import ui.ConstMIDP;
+import ui.controls.NumberField;
+
+class AccountForm implements CommandListener, ItemStateListener {
+    
+    private final AccountSelect accountSelect;
+    
+    private Display display;
+    private Displayable parentView;
+    
+    private Form f;
+    private TextField userbox;
+    private TextField passbox;
+    private TextField servbox;
+    private TextField ipbox;
+    private NumberField portbox;
+    private TextField resourcebox;
+    private TextField nickbox;
+    private TextField proxyHost;
+    private NumberField proxyPort;
+    private ChoiceGroup register;
+    
+    Command cmdOk = new Command("Ok", Command.OK, 1); //locale
+    Command cmdPwd = new Command(SR.MS_SHOWPWD, Command.SCREEN, 2); //locale
+    Command cmdCancel = new Command(SR.MS_BACK /*"Back"*/, Command.BACK, 99); //locale
+    
+    Account account;
+    
+    boolean newaccount;
+    
+    public AccountForm(AccountSelect accountSelect, Display display, Account account) {
+	this.accountSelect = accountSelect;
+	this.display=display;
+	parentView=display.getCurrent();
+	
+	newaccount= account==null;
+	if (newaccount) account=new Account();
+	this.account=account;
+	
+	String title = (newaccount)?
+	    SR.MS_NEW_ACCOUNT /*"New Account"*/:  //locale
+	    (account.toString());
+	f = new Form(title);
+	userbox = new TextField("Username", account.getUserName(), 32, TextField.URL); f.append(userbox); //locale
+	passbox = new TextField("Password", account.getPassword(), 32, TextField.PASSWORD);	f.append(passbox); //locale
+        passStars(false);
+	servbox = new TextField(SR.MS_SERVER,   account.getServer(),   32, TextField.URL); f.append(servbox); //locale
+	ipbox = new TextField(SR.MS_HOST_IP, account.getHostAddr(), 32, TextField.URL);	f.append(ipbox); //locale
+	portbox = new NumberField(SR.MS_PORT, account.getPort(), 0, 65535); f.append(portbox); //locale
+	register = new ChoiceGroup(null, Choice.MULTIPLE);
+	register.append("use SSL",null); //locale
+	register.append(SR.MS_PLAIN_PWD,null); //locale
+	register.append(SR.MS_SASL,null); //locale
+	register.append("conferences only",null); //locale
+	register.append(SR.MS_PROXY_ENABLE,null); //locale
+	register.append(SR.MS_REGISTER_ACCOUNT,null); //locale
+	boolean b[] = {account.getUseSSL(), account.getPlainAuth(), account.isSASL(), account.isMucOnly(), account.isEnableProxy(), false};
+	
+	register.setSelectedFlags(b);
+	f.append(register);
+        
+	proxyHost = new TextField(SR.MS_PROXY_HOST,   account.getProxyHostAddr(),   32, TextField.URL); f.append(proxyHost); //locale
+	proxyPort = new NumberField(SR.PROXY_PORT, account.getProxyPort(), 0, 65535);	f.append(proxyPort); //locale
+        
+	resourcebox = new TextField(SR.MS_RESOURCE, account.getResource(), 32, TextField.ANY); f.append(resourcebox); //locale
+	nickbox = new TextField("Account name", account.getNickName(), 32, TextField.ANY); f.append(nickbox); //locale
+	
+	f.addCommand(cmdOk);
+        f.addCommand(cmdPwd);
+	f.addCommand(cmdCancel);
+	
+	f.setCommandListener(this);
+	f.setItemStateListener(this);
+	
+	display.setCurrent(f);
+    }
+    
+    private void passStars(boolean force) {
+	if (passbox.size()==0 || force)
+	    passbox.setConstraints(TextField.ANY | ConstMIDP.TEXTFIELD_SENSITIVE);
+        fixPassBugWEME();
+    }
+    
+    private String fixPassBugWEME(){
+        String newPass=passbox.getString();
+        String oldPass=account.getPassword();
+        
+        if (oldPass!=null)
+            if (oldPass.length()==newPass.length() && newPass.startsWith("**") && newPass.endsWith("**")) {
+                newPass=oldPass;
+                passbox.setString(oldPass);
+            }
+        return newPass;
+    }
+    
+    public void itemStateChanged(Item item) {
+	if (item==userbox) {
+	    String user = userbox.getString();
+	    int at = user.indexOf('@');
+	    if (at==-1) return;
+	    //userbox.setString(user.substring(0,at));
+	    servbox.setString(user.substring(at+1));
+	}
+	if (item==passbox) passStars(false);
+    }
+    
+    public void commandAction(Command c, Displayable d) {
+	if (c==cmdCancel) {
+	    destroyView();
+	    return;
+	}
+	if (c==cmdOk) {
+	    boolean b[] = new boolean[6];
+	    register.getSelectedFlags(b);
+	    String user = userbox.getString();
+	    int at = user.indexOf('@');
+	    if (at!=-1) user=user.substring(0, at);
+	    account.setUserName(user.trim());
+            
+	    account.setPassword(fixPassBugWEME());
+            
+	    account.setServer(servbox.getString().trim());
+	    account.setHostAddr(ipbox.getString());
+	    account.setResource(resourcebox.getString());
+	    account.setNickName(nickbox.getString());
+	    account.setUseSSL(b[0]);
+	    account.setPlainAuth(b[1]);
+//#if SASL
+            account.setSasl(b[2]);
+//#endif
+	    account.setMucOnly(b[3]);
+	    account.setEnableProxy(b[4]);
+	    //account.updateJidCache();
+	    
+	    account.setPort(portbox.getValue());
+
+	    account.setProxyHostAddr(proxyHost.getString());
+            account.setProxyPort(proxyPort.getValue());
+	    
+	    if (newaccount) accountSelect.accountList.addElement(account);
+	    accountSelect.rmsUpdate();
+	    accountSelect.commandState();
+	    
+	    if (b[5])
+		new AccountRegister(account, display, parentView); 
+	    else destroyView();
+	}
+        if (c==cmdPwd) passStars(true);
+    }
+    
+    public void destroyView()	{
+	if (display!=null)   display.setCurrent(parentView);
+    }
+}
---
+++ micro/src/Client/ContactMessageList.java
@@ -0,0 +1,223 @@
+/*
+ * ContactMessageList.java
+ *
+ * Created on 19 Февраль 2005 г., 23:54
+ *
+ * Copyright (c) 2005-2006, Eugene Stahov (evgs), http://bombus.jrudevels.org
+ * All rights reserved.
+ */
+
+package Client;
+import Messages.MessageList;
+import Messages.MessageParser;
+import archive.MessageArchive;
+import images.RosterIcons;
+import locale.SR;
+import util.ClipBoard;
+import vcard.VCard;
+import ui.*;
+import java.util.*;
+import javax.microedition.lcdui.*;
+/**
+ *
+ * @author Eugene Stahov
+ */
+public class ContactMessageList extends MessageList
+{
+    
+    Contact contact;
+    Command cmdSubscribe=new Command(SR.MS_SUBSCRIBE, Command.SCREEN, 1);
+    Command cmdMessage=new Command(SR.MS_NEW_MESSAGE,Command.SCREEN,2);
+    Command cmdQuoteNick=new Command("Quote nickname",Command.SCREEN,3);
+    Command cmdResume=new Command(SR.MS_RESUME,Command.SCREEN,1);
+    Command cmdQuote=new Command(SR.MS_QUOTE,Command.SCREEN,4);
+    Command cmdArch=new Command(SR.MS_ADD_ARCHIVE,Command.SCREEN,5);
+    Command cmdPurge=new Command(SR.MS_CLEAR_LIST, Command.SCREEN, 6);
+    Command cmdContact=new Command(SR.MS_CONTACT,Command.SCREEN,7);
+    Command cmdActive=new Command(SR.MS_ACTIVE_CONTACTS,Command.SCREEN,7);
+    Command cmdCopy = new Command("Copy", Command.SCREEN, 8);
+    
+    private ClipBoard clipboard;
+    
+    StaticData sd;
+    
+    /** Creates a new instance of MessageList */
+    public ContactMessageList(Contact contact, Display display) {
+        super(display);
+        this.contact=contact;
+        sd=StaticData.getInstance();
+        
+        Title title=new Title(contact);
+        setTitleItem(title);
+        
+        title.addRAlign();
+        title.addElement(null);
+        title.addElement(null);
+        //setTitleLine(title);
+
+        cursor=0;//activate
+        
+        addCommand(cmdMessage);
+        addCommand(cmdQuoteNick);
+        addCommand(cmdPurge);
+        addCommand(cmdContact);
+	addCommand(cmdActive);
+        //if (getItemCount()>0) {
+            addCommand(cmdQuote);
+            addCommand(cmdArch);
+	//}
+        addCommand(cmdCopy);
+        setCommandListener(this);
+        moveCursorTo(contact.firstUnread(), true);
+        //setRotator();
+    }
+    
+    public void showNotify(){
+        super.showNotify();
+        if (cmdResume==null) return;
+        if (contact.msgSuspended==null) removeCommand(cmdResume);
+        else addCommand(cmdResume);
+        
+        if (cmdSubscribe==null) return;
+        try {
+            Msg msg=(Msg) contact.msgs.elementAt(cursor); 
+            if (msg.messageType==Msg.MESSAGE_TYPE_AUTH) addCommand(cmdSubscribe);
+            else removeCommand(cmdSubscribe);
+        } catch (Exception e) {}
+        
+    }
+    
+    protected void beginPaint(){
+        getTitleItem().setElementAt(sd.roster.messageIcon,2);
+        //getTitleItem().setElementAt(contact.incomingComposing, 3);
+    }
+    
+    public void markRead(int msgIndex) {
+	if (msgIndex>=getItemCount()) return;
+	//Msg msg=getMessage(msgIndex);
+        //if (msg.unread) contact.resetNewMsgCnt();
+        //msg.unread=false;
+        if (msgIndex<contact.lastUnread) return;
+        //if (contact.needsCount())
+            sd.roster.countNewMsgs();
+    }
+    
+    
+    public int getItemCount(){ return contact.msgs.size(); }
+    //public Element getItemRef(int Index){ return (Element) contact.msgs.elementAt(Index); }
+
+    public Msg getMessage(int index) { 
+	Msg msg=(Msg) contact.msgs.elementAt(index); 
+	if (msg.unread) contact.resetNewMsgCnt();
+	msg.unread=false;
+	return msg;
+    }
+    
+    public void focusedItem(int index){ 
+        markRead(index); 
+        /*try {
+            Msg msg=(Msg) contact.msgs.elementAt(index); 
+            if (msg.messageType==Msg.MESSAGE_TYPE_AUTH) addCommand(cmdSubscribe);
+            else removeCommand(cmdSubscribe);
+        } catch (Exception e) {}*/
+    }
+        
+    public void commandAction(Command c, Displayable d){
+        super.commandAction(c,d);
+        /*if (c==cmdBack) {
+            //contact.lastReaded=contact.msgs.size();
+            //contact.resetNewMsgCnt();            
+            destroyView();
+            return;
+        }*/
+        if (c==cmdMessage) { 
+            contact.msgSuspended=null; 
+            keyGreen(); 
+        }
+        if (c==cmdQuoteNick) {
+            try {
+                String Body=getMessage(cursor).getBody();
+                String nickname=Body.substring(0,Body.indexOf('>'));
+                new MessageEdit(display,contact,nickname+": ");
+            } catch (Exception e) {/*no messages*/}
+        }
+        if (c==cmdResume) { keyGreen(); }
+        if (c==cmdQuote) {
+            try {
+                new MessageEdit(display,contact,getMessage(cursor).toString());
+            } catch (Exception e) {/*no messages*/}
+        }
+        if (c==cmdArch) {
+            try {
+                MessageArchive.store(getMessage(cursor));
+            } catch (Exception e) {/*no messages*/}
+        }
+        if (c==cmdPurge) {
+            clearMessageList();
+        }
+        if (c==cmdContact) {
+            if (sd.roster.theStream!=null)
+                new RosterItemActions(display, contact);
+        }
+	
+	if (c==cmdActive) {
+	    new ActiveContacts(display, contact);
+	}
+        if (c == cmdCopy)
+        {
+            try {
+                clipboard.s=getMessage(cursor).getBody();
+            } catch (Exception e) {/*no messages*/}
+        }
+        
+        if (c==cmdSubscribe) {
+            if (contact.subscr==null) return;
+            boolean subscribe = 
+                    contact.subscr.startsWith("none") || 
+                    contact.subscr.startsWith("from");
+            if (contact.ask_subscribe) subscribe=false;
+
+            boolean subscribed = 
+                    contact.subscr.startsWith("none") || 
+                    contact.subscr.startsWith("to");
+                    //getMessage(cursor).messageType==Msg.MESSAGE_TYPE_AUTH;
+            
+            String to=contact.getBareJid();
+            
+            if (subscribed) sd.roster.sendPresence(to,"subscribed", null);
+            if (subscribe) sd.roster.sendPresence(to,"subscribe", null);
+
+        }
+    }
+
+    private void clearMessageList() {
+        //TODO: fix scrollbar size
+        moveCursorHome();
+        contact.purge();
+        messages=new Vector();
+        System.gc();
+        redraw();
+    }
+    
+    public void keyGreen(){
+        (new MessageEdit(display,contact,contact.msgSuspended)).setParentView(this);
+        contact.msgSuspended=null;
+    }
+    
+    public void keyRepeated(int keyCode) {
+	if (keyCode==KEY_NUM3) new ActiveContacts(display, contact);
+        if (keyCode==KEY_NUM0) clearMessageList();
+	else super.keyRepeated(keyCode);
+    }
+
+    public void userKeyPressed(int keyCode) {
+        super.userKeyPressed(keyCode);
+        if (keyCode==keyClear) {
+            //new YesNoAlert(display, this, SR.MS_CLEAR_LIST, SR.MS_SURE_CLEAR){
+               // public void yes() { 
+                    clearMessageList();
+                //}
+            //};
+        }
+    }
+}
